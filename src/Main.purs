c06b7a4a105763f14393d98cbf04d1303037aa8e
Alex Gryzlov <skyrodent@gmail.com>
2019-04-02 15:43:30 +0300

    add pairs and sums

--- src/Main.purs
+++ src/Main.purs
@@ -1,24 +1,20 @@
 module Main where
 
+import Prelude (Unit, bind, pure, unit, ($))
+--import Prelude (Unit, unit, ($), (<$>), (<>), (>>=), show, bind, pure, const, discard)
+--import Data.Either (Either)
 import Control.Monad.Except (runExcept)
 import Debug.Trace (spy)
 import Effect (Effect)
 --import Effect.Console (log, logShow)
 import Foreign.Index (readProp)
-import Foreign.Keys (keys)
-import Prelude (Unit, unit, ($), (<$>), (<>), (>>=), show, bind, pure, const, discard)
-import Foreign (typeOf, tagOf)
---import Data.Either (Either)
-
-import ForeignCtor (ctorName)
 import Typedefs (deserialize0, termFromForeign)
 
 main :: Effect Unit
 main = do
-  let full = spy "parsed" $ deserialize0 "1" "()"
+  let full = spy "parsed" $ deserialize0 "(+ 1 1)" "(right ())"
   let ee = spy "run" $ 
            runExcept $ do dpair <- readProp "$1" full 
                           term <- spy "term" $ readProp "$2" dpair
                           termFromForeign term
   pure unit
---  pure unit
--- src/Typedefs.purs
+++ src/Typedefs.purs
@@ -5,6 +5,7 @@ import Control.Monad.Error.Class (throwError)
 import Data.Function.Uncurried (Fn2, runFn2)
 import Data.Array (length)
 import Foreign (F, Foreign, ForeignError(..))
+import Foreign.Index (readProp)
 import Foreign.Keys (keys)
 import ForeignCtor (ctorName)
 
@@ -16,12 +17,25 @@ deserialize0 = runFn2 deserialize0Impl
 data TDTerm
   = TUnit
   | TLeft TDTerm
-  | TRIght TDTerm
+  | TRight TDTerm
   | TBoth TDTerm TDTerm
   | TInn TDTerm
 
 termFromForeign :: Foreign -> F TDTerm
 termFromForeign f = case ctorName f of 
-  "Object" -> do k <- keys f 
-                 if length k == 1 then pure TUnit else throwError $ pure $ ForeignError "unknown object"
+  "Object" -> do k <- keys f
+                 if length k == 1 
+                   then pure TUnit 
+                   else throwError $ pure $ ForeignError "unknown object"
+  "$HC_2_0$Builtins__MkPair" -> do fst <- readProp "$1" f
+                                   term1 <- termFromForeign fst
+                                   snd <- readProp "$1" f
+                                   term2 <- termFromForeign snd 
+                                   pure $ TBoth term1 term2
+  "$HC_1_0$Prelude__Either__Left" -> do fst <- readProp "$1" f
+                                        term1 <- termFromForeign fst
+                                        pure $ TLeft term1
+  "$HC_1_1$Prelude__Either__Right" -> do fst <- readProp "$1" f
+                                         term1 <- termFromForeign fst
+                                         pure $ TRight term1
   _ -> throwError $ pure $ ForeignError "unknown term"  
